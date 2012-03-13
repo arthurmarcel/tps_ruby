00e9e92e6a2e0849aa447b29aa5a87f7d55807ea
Arthur Marcel <arthur.marcel@etu.enseeiht.fr>
2012-03-13 15:08:38 +0100

    s12

---
+++ s10/testapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testapp::Application.config.secret_token = '10b4291dfc2678b534c8757a45b5571179ed583f0bb1b98cd886a43a5abca880a796925eea60ea4c639f6523a7961544558836981bb09ab5f14cb31df368b321'
