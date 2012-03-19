498e3ad29d88673ca57eb7b4d742ed034f7df4b0
Arthur Marcel <arthur.marcel@etu.enseeiht.fr>
2012-03-19 20:03:23 +0100

    s14

---
+++ s14/todojs/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todojs::Application.config.secret_token = '28744919706da8695433e7405c7214098fcb2aa24ee5249d1443af9aae144eb4fde92238d3dbe09dc4ef2a48a7bfd06a5ef4d7ec22144bef4a917b8dea4482bf'
